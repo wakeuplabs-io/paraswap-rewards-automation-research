dc854b62cf12eb1b14f185078843c7a62d1a04c4
Matias Barro <matias.barro@gmail.com>
2025-01-06 09:10:08 -0300

    improve event listener

--- app/paraswap-event-processor/index.ts
+++ app/paraswap-event-processor/index.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
-import { createPublicClient, http, parseAbiItem } from 'viem';
+import { createPublicClient, hexToBigInt, http, parseAbiItem } from 'viem';
 import { mainnet } from 'viem/chains';
+import { z } from 'zod';
 
 const PARASWAP_AUGUSTUS_RFQ_MAINNET =
   '0xe92b586627cca7a83dc919cc7127196d70f55a06';
@@ -10,6 +11,21 @@ const ORDER_FILLED_EVENT =
 
 const orderFilledEventAbi = parseAbiItem([ORDER_FILLED_EVENT]);
 
+// Data structure: https://developers.paraswap.network/api/limit-orders/data-structure-in-our-centralized-system
+
+const schema = z.object({
+  orderHash: z.string(),
+  maker: z.string(),
+  makerAsset: z.string(),
+  makerAmount: z.bigint(),
+  taker: z.string(),
+  takerAsset: z.string(),
+  takerAmount: z.bigint(),
+  timeStamp: z.bigint().transform((val) => dayjs.unix(Number(val)).toString()),
+  block: z.bigint(),
+  gasUsed: z.bigint(),
+});
+
 export const publicClient = createPublicClient({
   chain: mainnet,
   transport: http(),
@@ -17,31 +33,25 @@ export const publicClient = createPublicClient({
 
 const lastBlock = await publicClient.getBlock();
 
-const log = (
-  await publicClient.getLogs({
-    address: PARASWAP_AUGUSTUS_RFQ_MAINNET,
-    event: orderFilledEventAbi,
-    toBlock: lastBlock.number,
-    fromBlock: lastBlock.number - 100n,
+const logs = await publicClient.getLogs({
+  address: PARASWAP_AUGUSTUS_RFQ_MAINNET,
+  event: orderFilledEventAbi,
+  toBlock: lastBlock.number,
+  fromBlock: lastBlock.number - 1000n,
+});
+
+const ordersFilled = await Promise.all(
+  logs.map(async (log) => {
+    const transactionReceipt = await publicClient.getTransactionReceipt({
+      hash: log.transactionHash,
+    });
+    return {
+      ...log.args,
+      gasUsed: transactionReceipt.gasUsed,
+      block: transactionReceipt.blockNumber,
+      timeStamp: hexToBigInt((log as any)['blockTimestamp']),
+    };
   })
-)[0];
+);
 
-const [transactionReceipt, transactionBlock] = await Promise.all([
-  publicClient.getTransactionReceipt({
-    hash: log.transactionHash,
-  }),
-  publicClient.getBlock({
-    blockHash: log.blockHash,
-  }),
-]);
-
-const jsonLog = {
-  order: log.args.orderHash,
-  maker: log.args.maker,
-  makerAsset: log.args.makerAsset,
-  makerAmount: log.args.makerAmount,
-  date: dayjs.unix(Number(transactionBlock.timestamp)).toString(),
-  gasUsed: transactionReceipt.gasUsed,
-};
-
-console.log(jsonLog);
+console.log(ordersFilled.map((order) => schema.parse(order)));
